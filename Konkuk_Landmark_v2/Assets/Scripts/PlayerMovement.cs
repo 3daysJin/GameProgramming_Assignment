ec4a1a059a2fa2ef04c37f5eb9b93fcee1f1ca48
DESKTOP-BJNECTQ\Lee Seung jin <3daysjin@gmail.com>
2023-09-20 17:21:47 +0900

    시작시 마우스를 가두는 기능 추가

--- Konkuk_Landmark_v2/Assets/Scripts/PlayerMovement.cs
+++ Konkuk_Landmark_v2/Assets/Scripts/PlayerMovement.cs
@@ -9,7 +9,8 @@ public class Player_Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
